ddd34b815bbe7620697ec60ceb52a4005e0d6145
Simul <simulbista@gmail.com>
2023-02-22 18:52:44 -0500

    sign up page - icons updated (minor css changes)

--- app/src/main/java/com/codecrafters/cookingapp/MainActivity.java
+++ app/src/main/java/com/codecrafters/cookingapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_13_history);
+        setContentView(R.layout.activity_5_signup);
     }
 }
