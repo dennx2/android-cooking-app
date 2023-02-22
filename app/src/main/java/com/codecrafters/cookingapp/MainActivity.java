7e8c6772da238586cb8d1c0d1e21843ba0979dcc
Simul <simulbista@gmail.com>
2023-02-21 21:43:41 -0500

    search-result page version1 commit (minor positioning changes to survey page)

--- app/src/main/java/com/codecrafters/cookingapp/MainActivity.java
+++ app/src/main/java/com/codecrafters/cookingapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_3_survey);
+        setContentView(R.layout.activity_10_search_result);
     }
 }
