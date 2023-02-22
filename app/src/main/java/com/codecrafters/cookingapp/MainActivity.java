9291cdf8998d17738bc4df07f00e420d410c0e0e
Simul <simulbista@gmail.com>
2023-02-21 19:32:50 -0500

    survey page removed the unwanted header vertical spacing by making adjustments in the guidelines

--- app/src/main/java/com/codecrafters/cookingapp/MainActivity.java
+++ app/src/main/java/com/codecrafters/cookingapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_5_signup);
+        setContentView(R.layout.activity_3_survey);
     }
 }
