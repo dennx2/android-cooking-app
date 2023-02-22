efaeb291e3f350702706f5e33460a77afa4e5a3d
Simul <simulbista@gmail.com>
2023-02-22 17:50:34 -0500

    recipe history page version1 commit

--- app/src/main/java/com/codecrafters/cookingapp/MainActivity.java
+++ app/src/main/java/com/codecrafters/cookingapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_10_search_result);
+        setContentView(R.layout.activity_13_history);
     }
 }
