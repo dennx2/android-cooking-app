a6397158bf02064d0f2b304771c230664a885b1a
dennx2 <jdcht1228@gmail.com>
2023-04-18 19:47:52 -0400

    solved conflicting testing module in gradle file, updated pg9, pg12, and Search class

--- app/src/main/java/com/codecrafters/cookingapp/pg09_RecommendationActivity.java
+++ app/src/main/java/com/codecrafters/cookingapp/pg09_RecommendationActivity.java
@@ -2,13 +2,17 @@ package com.codecrafters.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class pg09_RecommendationActivity extends AppCompatActivity {
+
+    Context context;
 
     EditText ingredient1;
     EditText ingredient2;
@@ -20,16 +24,18 @@ public class pg09_RecommendationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pg09_recommendation);
 
+        context = getApplicationContext();
+    }
+
+    public void recommendMeClicked(View view) {
         ingredient1 = findViewById(R.id.tv1_2);
         ingredient2 = findViewById(R.id.tv2_2);
         ingredient3 = findViewById(R.id.tv3_2);
-    }
 
-    public void recommendMeClicked() {
         String searchString = ingredient1.getText().toString() + ingredient2.getText().toString() + ingredient3.getText().toString();
 
         // TODO Perform search. For now I'm using existing JSON file, so the String from TExtView will not be used.
-        ArrayList<Recipe> recipeList = Search.ingredientSearch();
+        ArrayList<Recipe> recipeList = Search.ingredientSearch(context);
 
         // Build an explicit Intent
         // TODO change destination class to Search Result Activity class
--- app/src/main/java/com/codecrafters/cookingapp/pg12_CountryCategoryActivity.java
+++ app/src/main/java/com/codecrafters/cookingapp/pg12_CountryCategoryActivity.java
@@ -1,24 +1,25 @@
 package com.codecrafters.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class pg12_CountryCategoryActivity extends AppCompatActivity {
 
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pg12_country_category);
+
+        context = getApplicationContext();
     }
 
 
@@ -29,7 +30,7 @@ public class pg12_CountryCategoryActivity extends AppCompatActivity {
         String countryName = textView.getText().toString();
 
         // TODO Perform search. For now I'm using existing JSON file, so the String from TExtView will not be used.
-        ArrayList<Recipe> recipeList = Search.countrySearch();
+        ArrayList<Recipe> recipeList = Search.countrySearch(context);
 
         // Build an explicit Intent
         // TODO change destination class to Search Result Activity class
